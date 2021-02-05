42f1877cf813efd8ade4f2453c369c155a128080
Brian Cook <brybcook@gmail.com>
2021-02-05 17:09:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,10 +5,10 @@ lowest_temperatures=[]
 row_index=0 
 while row_index < src.count do
   element_index=0 
-  low_temp=100
+  low_temp=
   while element_index < src[row_index].count do
     if src[row_index][element_index] > low_temp
-      low_temp=src[rrow_index][element_index]
+      low_temp=src[row_index][element_index]
     end
     element_index+=1
   end 
